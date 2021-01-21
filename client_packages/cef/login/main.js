247a0213bff61bfcd7d821f9f3f8f3da534333b9
aksiere <33295331+aksiere@users.noreply.github.com>
2021-01-21 19:24:06 +0300

    Update main.js
    prevent submit to avoid update page & no error notify

--- client_packages/cef/login/main.js
+++ client_packages/cef/login/main.js
@@ -1,22 +1,25 @@
 $('.throwError').hide()
 
-mp.events.add('b.throwError', (err) => {
-	$('.throwError').show().html(err)
+$('form').on('submit', function (e) {   
+    e.preventDefault()
 })
 
 function sendAccountInfo(state) {
 	switch (state) {
 		case 0:
-			mp.events.call('client:loginData', $('#loginName').val(), $('#loginPass').val())
+			mp.events.call('c.loginData', $('#loginName').val(), $('#loginPass').val())
 			break
 		case 1:
-			if ($('#registerPass').val() === $('#registerPass2').val()) {
-				mp.events.call('client:registerData', $('#registerName').val(), $('#registerEmail').val(), $('#registerPass').val())
-			} else {
-				mp.events.call('b.throwError', 'password-mismatch')
-			}
-			break
-		default:
+			if ($('#registerPass').val() !== $('#registerPass2').val()) return throwError('password-mismatch')
+			mp.events.call('c.registerData', $('#registerName').val(), $('#registerEmail').val(), $('#registerPass').val())
 			break
 	}
 }
+
+function throwError(err) {
+	$('.throwError').show().html(err)
+}
+
+mp.events.add('b.throwError', (err) => {
+	throwError(err)
+})
