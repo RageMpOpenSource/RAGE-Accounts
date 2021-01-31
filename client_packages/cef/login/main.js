1bcf9204610145d95430cc7b7541c07d729d1438
aksiere <33295331+aksiere@users.noreply.github.com>
2021-01-31 04:14:25 +0300

    Update main.js

--- client_packages/cef/login/main.js
+++ client_packages/cef/login/main.js
@@ -1,25 +1,31 @@
-$('.throwError').hide()
+$('.alert').hide();
 
-$('form').on('submit', function (e) {   
-    e.preventDefault()
+$('form').submit(function(e){
+	e.preventDefault()
 })
 
 function sendAccountInfo(state) {
+	$('.alert').hide();
 	switch (state) {
 		case 0:
-			mp.events.call('c.loginData', $('#loginName').val(), $('#loginPass').val())
-			break
+			mp.events.call('client:loginData', $('#loginName').val(), $('#loginPass').val());
+			break;
 		case 1:
-			if ($('#registerPass').val() !== $('#registerPass2').val()) return throwError('password-mismatch')
-			mp.events.call('c.registerData', $('#registerName').val(), $('#registerEmail').val(), $('#registerPass').val())
-			break
+			if ($('#registerPass').val() == $('#registerPass2').val()) {
+				mp.events.call('client:registerData', $('#registerName').val(), $('#registerEmail').val(), $('#registerPass').val());
+			} else {
+				throwError('password-mismatch');
+			}
+			break;
+		default:
+			break;
 	}
 }
 
 function throwError(err) {
-	$('.throwError').show().html(err)
+	$('.alert').show().html(err);
 }
 
 mp.events.add('b.throwError', (err) => {
-	throwError(err)
+	throwError(err);
 })
